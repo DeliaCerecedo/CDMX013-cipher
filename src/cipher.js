34622746b9197a563480d7140ba64690d1d85d63
DeliaCerecedo <dhdiaz1971@gmail.com>
2022-07-25 09:53:01 -0500

    vienticinco de julio

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
-const cipher = {
-  // ...
-};
+function cipher() {
 
-export default cipher;
+  
+
+}
+  export default cipher
+
+/*implementar el objeto cipher debe contener dos métodos: 
+cipher.encode(offset, string)
+cipher.decode(offset, string)
+
+
+ 
+  alert("estoy en la funcion cipher.js");
+  readOffset();
+  readText();
+  cifrarOriginalText();
+ 
+}
+
+
+
+const readText=function(){
+  alert("estoy en la funcion leer texto en cipher.js");
+  const originalText=document.getElementById("textParaCifrar").value;
+  alert(originalText);
+ 
+}
+
+
+
+const readOffset=function(){
+  const offsetSelected=document.getElementById("offset").value;
+  alert("estoy leyendo el offset en cipher.js")
+  alert(offsetSelected);
+}
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
-import cipher from './cipher.js';
 
-console.log(cipher);
+import cipher from "./cipher.js";
+
+/*invocar aquí a cipher.code(offset, string) y cipher.decode() y actualiza los resultados en la UI*/ 
+
+
+const cipher = {
+    code: function string(){
+        alert("estoy en la el objeto cipher.code leer texto en cipher.js");
+        const originalText=document.getElementById("textParaCifrar").value;
+        alert(originalText);
+
+}
+    /*decode: [offset, string]
+  }
+*/
+
+
+function readText(){
+    alert("estoy en la funcion leer texto en cipher.js");
+    /*const string=document.getElementById("textParaCifrar").value;
+    alert(string);*/
+
+   
+}
+
+
+const cifrarElTexto=document.getElementById('botonCifrar');
+
+cifrarElTexto.addEventListener('click', readText);
+
+const string=document.getElementById("textParaCifrar").value;
+
+
+
+const pantallaCifrar=function(){
+    
+    document.getElementById('bienvenida').style.display = "none";
+    document.getElementById('pantallaCifrar').style.display = "block";
+
+};
+const botonBienvenidaCifrar=document.getElementById('botonBienvenidaCifrar');
+botonBienvenidaCifrar.addEventListener('click', pantallaCifrar);
+
+
+
+const irMenuPrincipal=function(){
+    document.getElementById('bienvenida').style.display = "block";
+    document.getElementById('pantallaCifrar').style.display = "none";
+ };
+ const menuPrincipal=document.getElementById('menuPrincipal');
+ menuPrincipal.addEventListener('click', irMenuPrincipal);
+
+
+
+
+
+/*
+
+
+
+const descifrar=function(){
+    document.getElementById("bienvenida").style.display = "none";
+    document.getElementById("pantallaDesifrar").style.display = "block";
+};
+
+const botonPantallaDescifrar=document.getElementById('botonPantallaDescifrar');
+botonDescifrar.addEventListener('click', descifrar);
+
+
+*/
+
+
